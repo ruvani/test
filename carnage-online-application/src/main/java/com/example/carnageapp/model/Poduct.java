a52ee2b7e46ec5f9382b584337268b92811af4cf
ruvani <ruvani.imesha@gmail.com>
2024-07-12 16:40:46 +0530

    Rename the class

--- carnage-online-application/src/main/java/com/example/carnageapp/model/Poduct.java
+++
@@ -1,5 +0,0 @@
-package com.example.carnageapp.model;
-
-public class Poduct {
-
-}
---
+++ carnage-online-application/src/main/java/com/example/carnageapp/model/Product.java
@@ -0,0 +1,29 @@
+package com.example.carnageapp.model;
+
+public class Product {
+
+	private int productCode;
+	private String name;
+	private double price;
+	
+	public int getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(int productCode) {
+		this.productCode = productCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
